dad742ab790ee2fcc25c303119e07e79ca3b8a77
galerp <32198237+galerp@users.noreply.github.com>
2023-06-27 16:39:09 -0400

    Update compose_prop.R

--- additionial_analyses/compose_prop.R
+++ additionial_analyses/compose_prop.R
@@ -6,11 +6,14 @@ setwd("/Users/galerp/Documents/manuscripts/cube3/git_repo/Cube3/Files/")
 #Set output directory
 output_dir = "/Users/galerp/Desktop/"
 
+#Get HPO ancestors file
 hpo_ancs <- read_csv("HPO_ancs_rl_2020-10-12_dl_2021-08-03.csv")
+
+#Your base file you wish to propagate
 base_hpo <- read_csv("example_bin_base.csv")
 
 
-prop_hpo<- base_hpo %>% 
+prop_hpo <- base_hpo %>% 
     left_join(hpo_ancs) %>% 
     select(ID, start_year, finish_year, ancs) %>% 
     rename(HPO = ancs) %>% 
