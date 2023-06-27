ed67698aab7695ff63c0c3e84e8e7e3a824cd722
galerp <32198237+galerp@users.noreply.github.com>
2023-06-27 16:35:01 -0400

    Update helper_functions.R

--- helper_functions.R
+++ helper_functions.R
@@ -2,9 +2,9 @@ library(tidyverse, quietly = T)
 
 pat_prop <- function(pat_base){
   pat_table_prop <- pat_base %>% 
-    left_join(hpo_ancs %>% dplyr::select(-definition)) %>% 
-    dplyr::select(famID, Ancestors) %>% 
-    dplyr::rename(HPO = Ancestors) %>% 
+    left_join(hpo_ancs %>% select(-definition)) %>% 
+    select(famID, Ancestors) %>% 
+    rename(HPO = Ancestors) %>% 
     separate_rows(HPO, sep = ";") %>% 
     #Remove duplicated HPO terms in each patient
     distinct()
